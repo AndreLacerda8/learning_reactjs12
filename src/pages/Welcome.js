742aab4625080d0c9d53a29ff37a0c056cee7e24
André Lacerda <andrlacerda180@gmail.com>
2022-01-03 14:14:46 -0300

    use Redirect component

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,12 @@
+import { Route } from 'react-router-dom'
+
 export function Welcome(){
   return(
-    <h1>Welcome</h1>
+    <section>
+      <h1>Welcome</h1>
+      <Route path='/welcome/new-user'>
+        <p>Welcome, new User</p>
+      </Route>    
+    </section>
   )
 }
